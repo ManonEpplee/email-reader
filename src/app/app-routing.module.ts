22652a538dd473fdef4dd19e591b6a0e53062108
Manon Epplée <manon.epplee@gmail.com>
2022-09-28 11:36:01 +0200

    add dropdown and routes to choose between exercises

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmailReaderBasicComponent } from './email-reader-basic/email-reader-basic.component';
+import { EmailReaderFormComponent } from './email-reader-form/email-reader-form.component';
 
 const routes: Routes = [
-  { path: 'email-reader-basic', component: EmailReaderBasicComponent },
+  { path: 'emailreaderbasic', component: EmailReaderBasicComponent },
+  { path: 'emailreaderform', component: EmailReaderFormComponent },
 ];
 
 @NgModule({
